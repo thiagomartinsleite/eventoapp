ddada01d21d49eeae4167db2c74827ae0910b2ee
thiagomartinsleite <thiagotho@gmail.com>
2019-03-14 21:17:43 -0300

    iniciando repositorio

---
+++ src/main/java/com/eventoapp/eventoapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.eventoapp.eventoapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EventoappApplication.class);
+	}
+
+}
